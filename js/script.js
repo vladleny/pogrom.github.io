d809332861802f176547c68900c4a1a2678bb586
vladleny <vlad.provotar@gmail.com>
2021-05-26 21:45:05 +0300

    pogrom

---
+++ js/script.js
@@ -0,0 +1,51 @@
+
+$(document).ready(function () {
+    $('.page').fullpage({
+        css3: true,
+        scrollingSpeed: 1300
+    });
+});
+$(function () {
+    $('.header__burger').click(function () {
+        $('.header__burger').toggleClass('active'),
+            $('.menu__row').toggleClass('active')
+    })
+});
+
+$(document).ready(function () {
+    $('.slider').slick({
+        arrows: false,
+        dots: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        speed: 2000,
+        autoplaySpeed: 2000,
+        centerMode: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    centerMode: true,
+                    centerPadding: 100,
+
+                }
+            },
+            {
+                breakpoint: 450,
+                settings: {
+                    centerMode: true,
+                    centerPadding: 50,
+                }
+            },
+            {
+                breakpoint: 320,
+                settings: {
+                    centerMode: false,
+
+                }
+            }
+
+        ]
+    });
+});
